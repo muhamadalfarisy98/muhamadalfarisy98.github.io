15cc7c4c3b7ea79fd4a9975aa683cbc7f2bc7f85
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2021-11-04 13:14:26 +0700

    [ADD] jQuery handler

---
+++ profile/static/lib/jquery.js
@@ -0,0 +1,13 @@
+// wait for html rendered
+$(function() {
+    $("li.list-group-item:nth-child(n + 1)").click(function (e) { 
+        e.preventDefault();
+        // $("li.nav-item").toggleClass("red_color");
+        alert('Under construction as soon as possible, please wait :)');
+    });
+    // $("li.nav-item").click(function (e) { 
+    //     e.preventDefault();
+    //     // $("li.nav-item>nav-link").attr("href", "https://muhamadalfarisy98.github.io/myprofile");
+    //     // alert('Under construction, please hehe :)');
+    // });
+})
