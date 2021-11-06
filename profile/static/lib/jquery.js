ba6e1248268aefe891d0c017eaed62a77da31833
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2021-11-06 11:22:41 +0700

    [IMP] add handler jQuery submit button

--- profile/static/lib/jquery.js
+++ profile/static/lib/jquery.js
@@ -1,13 +1,18 @@
 // wait for html rendered
 $(function() {
     $("li.list-group-item:nth-child(n + 1)").click(function (e) { 
-        e.preventDefault();
+        e.preventDefault(); // prevent event object happens
         // $("li.nav-item").toggleClass("red_color");
-        alert('Under construction as soon as possible, please wait :)');
+        alert('Under construction, please wait :)');
     });
     // $("li.nav-item").click(function (e) { 
     //     e.preventDefault();
     //     // $("li.nav-item>nav-link").attr("href", "https://muhamadalfarisy98.github.io/myprofile");
     //     // alert('Under construction, please hehe :)');
     // });
+
+    $("input#submit").on("click", function () {
+        var name = $("input#name").val();
+        alert(`Thankyou ${name} ! But, sorry i am still managing the Backend side :D `);
+    });
 })
