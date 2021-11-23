e1e10f8b74881c22b1cb08e9991aec1f509e1058
muhamadalfarisy98 <muhamadalfarisy9d@yahoo.com>
2021-11-23 11:15:05 +0700

    [IMP] some changes :
    - add new medium : love letter
    - imp view

--- profile/static/lib/jquery.js
+++ profile/static/lib/jquery.js
@@ -1,6 +1,11 @@
 // wait for html rendered
 $(function() {
-    $("li.list-group-item:nth-child(n + 1)").click(function (e) { 
+    // $("li.list-group-item:nth-child(n + 1)").click(function (e) { 
+    //     e.preventDefault(); // prevent event object happens
+    //     // $("li.nav-item").toggleClass("red_color");
+    //     alert('Under construction, please wait :)');
+    // });
+    $(".ongoing").click(function (e) { 
         e.preventDefault(); // prevent event object happens
         // $("li.nav-item").toggleClass("red_color");
         alert('Under construction, please wait :)');
